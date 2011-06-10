f3f3bb59162c64c1eb5b5a595f355784a0eb4f27
Danny Bouman <dannybb@gmail.com>
2011-06-10 17:40:49 -0400

    Added test suite based on PHPUnit framework

---
+++ tests/EtapestryAPI_Test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'EtapestryAPI_Test_Config.php';
+
+class EtapestryAPI_Test extends PHPUnit_Framework_TestCase
+{
+    
+    public function testLogin()
+    {
+        // write login test here
+    }
+    
+}
---
+++ tests/EtapestryAPI_Test_Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Configuration for Etapestry API Tests
+ */
+
+/**
+ * Enter your eTapestry API account credentials to run tests
+ */
+defined('ETAPESTRYAPI_LOGIN_ID',"");
+define("ETAPESTRYAPI_PASSWORD", "");
+define("ETAPESTRYAPI_ENDPOINT", "");
+
+require_once dirname(dirname(__FILE__)) . '/EtapestryAPI.php';
+require_once 'PHPUnit/Framework.php';
+
+if (ETAPESTRYAPI_LOGIN_ID == "") {
+    die('Enter your eTapestry API account credentials in '.__FILE__.' before running the test suite.');
+}
+
+?>
